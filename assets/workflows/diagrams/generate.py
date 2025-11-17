6d34c26828e173416ca067b51ee2a604cc5dc2fb
Lukas Bindreiter <lukas.bindreiter@tilebox.com>
2025-11-17 16:36:55 +0100

    Remove mention of 64 subtasks limit (#86)

--- assets/workflows/diagrams/generate.py
+++ assets/workflows/diagrams/generate.py
@@ -27,39 +27,31 @@ def generate_light_and_dark_svgs(diagram_file: Path, output_dir: Path):
 def to_dark(diagram: str) -> str:
     """Converts a diagram to dark mode by manipulating colors."""
 
-    # dark background color
-    diagram = diagram.replace('style.fill: "#fcf9fa"', 'style.fill: "#161416"')
-    # white title text color
-    diagram = diagram.replace('style.font-color: "black"', 'style.font-color: "white"')
-
-    # replace all fill colors depending on the state of the task:
-    lookup = {
-        "HoneyDew": ("#265429", "#C7E6C9"),  # computed
-        "LavenderBlush": ("#A37200", "#FFE099"),  # queued
-        "PaleTurquoise": ("#3E7079", "#AED0D5"),  # running
-        "Salmon": ("#A31800", "#FF9785"),  # failed
+    color_mapping = {
+        # general
+        "#000000": "#FFFFFF",
+        # main background color
+        "#FCF9FA": "#161416",
+        # title text, parent->child edge color
+        "#170206": "#F4F1F4",
+        # dependency edge color
+        "#9B1A47": "#F97F76",
+        # queued
+        "#FFF0F5": "#A37200",
+        "#504448": "#fcc76f",
+        # running
+        "#AFEEEE": "#3E7079",
+        "#0e5253": "#B1E5EF",
+        # computed
+        "#F0FFF0": "#265429",
+        "#3F4B40": "#B7EBB8",
+        # failed
+        "#FA8072": "#A31800",
+        "#4A1511": "#F78D79",
     }
 
-    for task_color, replace_colors in lookup.items():
-        fill_color, stroke_color = replace_colors
-        diagram = diagram.replace(
-            f"style.fill: {task_color}",
-            f'style.fill: "{fill_color}"\n  style.stroke: "{stroke_color}"\n  style.font-color: "white"',
-        )
-
-    # replace dependency edges with another color
-    dependency_edges = re.findall(
-        r"(([0-9a-zA-Z-]+ <- [0-9a-zA-Z-]+): \{.*)\n", diagram
-    )
-    for edge, relation in dependency_edges:
-        diagram = diagram.replace(
-            edge, relation + ': {style: {stroke: "#F97F76"; stroke-dash: 3}}'
-        )
-
-    # make subtask edges white
-    subtask_edges = re.findall(r"([0-9a-zA-Z-]+ -> [0-9a-zA-Z-]+)\n", diagram)
-    for edge in subtask_edges:
-        diagram = diagram.replace(edge, edge + ": {style.stroke: white}")
+    for light_color, dark_color in color_mapping.items():
+        diagram = diagram.replace(light_color, dark_color)
 
     return diagram
 
